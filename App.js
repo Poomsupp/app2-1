408eb4fe9d7c98824b50b637a28b1234c61824e8
Poomsupp <264318241008-st@rmutsb.ac.th>
2023-07-25 11:55:09 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import react from 'react'
+//import ArrayMap from './array-map';
+//import ColumnItem from './column-item';
+//import RowItem from './row-item';
+import LayoutItem from './Layout-item';
+import Question from './Question';
+import Image from './image';
+
+export default function App() {
+  return (
+      [<Question/>,<Image/>,<LayoutItem/>]
+  );
+}
